f573318a1e8a7d5b70697bd24de3d27428134a00
alihaidersiddique <alihaidersiddique@hotmail.com>
2021-12-15 14:58:07 +0500

    home screen created

---
+++ lib/presentation/Example/example.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ExampleClass extends StatelessWidget {
+  const ExampleClass({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
